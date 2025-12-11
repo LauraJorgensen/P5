d0ac9bdad8385c87430a2a868046725d018d0501
Laura Jørgensen <ljorg23@student.aau.dk>
2025-12-11 21:46:32 +0100

    Update stochastic_process_hour_12.py

--- figures/stochastic_process_hour_12.py
+++ figures/stochastic_process_hour_12.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("data/pv_production_june_clean.csv", parse_dates=["timestamp"])
 df.set_index("timestamp", inplace=True)
 y = df["pv_production"]
+times = pd.DatetimeIndex(data['timestamp'])
 
 # vælg 7 dage til markering
 unique_days = sorted(list({t.date() for t in y.index}))
@@ -17,36 +18,35 @@ y_week = y.loc[start:end]
 
 # få time-12 værdier
 y_hourly = y.resample("H").mean()
-y_hour12 = y_hourly[y_hourly.index.hour == 12]   
-y_hour12_week = y_hour12.loc[start:end]         
+y_hour12 = y_hourly[y_hourly.index.hour == 12]
+y_hour12_week = y_hour12.loc[start:end]
 
 
-plt.figure(figsize=(10, 3))
-
-# beregn 12:00-tidspunkter og værdier fra den allerede udvalgte serie
+# plot 1
+plt.figure(figsize=(10, 4))
 twelve_times = y_hour12_week.index
 twelve_vals  = y_hour12_week.values
-
-
-# tegn orange prikker ovenpå linjen 
-
-plt.plot(y_week.index, y_week.values, linewidth=1, label="PV Production")
-plt.scatter(twelve_times, twelve_vals, color='orange', zorder=5, s=50, marker='o', label="Samples at 12:00")
-
-
+plt.plot(y_week.index,y_week.values,linewidth=1.2,color="tab:blue",label="Normalized PV Production")
+plt.scatter(twelve_times,twelve_vals,color="tab:orange",s=40,marker='o',zorder=5,label="Samples at 12:00")
 plt.ylim(0, 1)
+plt.xlim(start, end)
+plt.grid(True)
 plt.xlabel("Time")
-plt.ylabel("Normalized Production")
-plt.legend()
+plt.ylabel("Normalized PV Production")
+plt.legend(frameon=False)
 plt.tight_layout()
-plt.show()
+plt.savefig(f"plot1_kl12.pdf")
+plt.close()
 
-# Tegn kun time-12 værdierne, fremhæv de 7 udvalgte dage
-
-plt.figure(figsize=(10, 3))
-plt.scatter(y_hour12.index, y_hour12.values, s=50, label="Other days", zorder=1)
-plt.scatter(y_hour12_week.index, y_hour12_week.values, color="orange", s=50, label="Selected 12:00 samples")
+# plot 2
+plt.figure(figsize=(10, 4))
+plt.scatter(y_hour12.index,y_hour12.values,s=40,alpha=0.6,)
+plt.scatter(y_hour12_week.index, y_hour12_week.values, color="tab:orange", s=40, zorder=5)
+plt.grid(True)
 plt.xlabel("Time")
-plt.ylabel("PV production")
+plt.ylim(0, 1)
+plt.ylabel("Normalized PV Production")
+plt.xlim(times[0], times[-1])
 plt.tight_layout()
-plt.show()
+plt.savefig(f"plot2_kl12.pdf")
+plt.close()
