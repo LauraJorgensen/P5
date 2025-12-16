0236038e52e481302518fe95e5f05a73e4faf03c
Laura Jørgensen <ljorg23@student.aau.dk>
2025-12-16 22:54:21 +0100

    Update and rename Baseline.py to Clear_sky_model.py

--- figures/Clear_sky_model.py
+++ figures/Clear_sky_model.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pandas as pd
-from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 import pvlib
 
-# --- Load PV production data ---
 data = pd.read_csv('data/pv_production_june_clean.csv', parse_dates=['timestamp'])
 P_full = data['pv_production'].values
 times = pd.DatetimeIndex(data['timestamp'])
 
-# --- Calculate baseline ---
+# --- Clear sky model ---
 latitude = 48.669
 longitude = 12.692
 tz = 'Europe/Berlin'
@@ -17,29 +16,14 @@ tilt = 20
 azimuth = 180
 clearsky = site.get_clearsky(times)
 solpos = site.get_solarposition(times)
-poa = pvlib.irradiance.get_total_irradiance(
-    surface_tilt=tilt,
-    surface_azimuth=azimuth,
-    solar_zenith=solpos['apparent_zenith'],
-    solar_azimuth=solpos['azimuth'],
-    dni=clearsky['dni'],
-    ghi=clearsky['ghi'],
-    dhi=clearsky['dhi'],
-)
+poa = pvlib.irradiance.get_total_irradiance(surface_tilt=tilt,surface_azimuth=azimuth,solar_zenith=solpos['apparent_zenith'],solar_azimuth=solpos['azimuth'],dni=clearsky['dni'],ghi=clearsky['ghi'],dhi=clearsky['dhi'],)
 b_full = poa['poa_global'].values
-b_full = b_full / np.max(b_full)  # Normalize
+b_full = b_full / np.max(b_full)
 
-# --- Filter for P_t > 0 and b_t > 0 ---
-mask = (P_full > 0) & (b_full > 0)
-P = P_full[mask]
-b = b_full[mask]
-
-# --- Divide data by normalized baseline ---
+# --- Compute ratio between clear sky and data ---
 normalized_ratio = np.where(b_full > 0, P_full / b_full, np.nan)
 
-# --- Create plot ---
-import matplotlib.pyplot as plt
-
+# --- Plot ---
 plt.figure(figsize=(10, 4))
 plt.plot(times, normalized_ratio)
 plt.xlabel('Time')
